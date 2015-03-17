2646067a7bb0fe31c5c2912c59e1fa2e6ce2c777
Ivan <ivan@yandex.ru>
2015-03-17 18:16:31 +0500

    Сопротивление ветра

--- src/Packet.java
+++ src/Packet.java
@@ -7,17 +7,20 @@ import java.util.Queue;
  * Математическая модель снаряда
  */
 public class Packet {
-	public final double G = 9.8;
+	//характеристика шара
 	public final double RADIUS_PIX = 10.0; //радиус шара в пикселях
-	public final double RADIUS = 1; //Радиус шара в метрах
+	public final double RADIUS = 1.0; //радиус шара в метрах
+	public final double S = RADIUS * RADIUS * Math.PI; //площадь сечения шара
+	public final double DENSITY = 7800; //плотность шара
+	public final double WEIGHT = 4/3*RADIUS*S*DENSITY;
+	//характеристика среды
+	public final double G = 9.8;
 	public final double L = 0.0065; //просто константа
 	public final double R = 8.314; //еще одна константа
 	public final double T0 = 288.15; //температура на уровне моря
 	public final double P0 = 101325; //Давление на уровне моря
 	public final double M = 0.029; //молярная масса воздуха
-	public final double Cf = 0.47; //Коэффициент для вычисления сопротивления воздуха
-	public final double S = RADIUS * RADIUS * Math.PI; //Площадь сечения шара
-	public double weight = 32000.0;
+	public final double Cf = 0.47; //коэффициент для вычисления сопротивления воздуха
 	
 	volatile private Point2D mPosition;
 	volatile private Point2D mSpeed, mAcceleration, mAirResistance, mGravity, mWindResistance;
@@ -40,8 +43,8 @@ public class Packet {
 		mPosition = new Point2D(0.0, 0.0);
 		mAcceleration = new Point2D(0.0, 0.0);
 		mAirResistance = new Point2D(0.0, 0.0);
-		mWindResistance = new Point2D(0.0, 0.0);
-		mGravity =  new Point2D(0.0, -weight* G);
+		mWindResistance = new Point2D(-10.6, 0.0);
+		mGravity =  new Point2D(0.0, -WEIGHT* G);
 		mTime = 0.0;
 		mSleepFactor = sleepFactor;
 		mLastDeltas = new ArrayDeque<>();
@@ -54,18 +57,25 @@ public class Packet {
 		flightRectangle = new Point2D(distanceInVacuum, maxHeightInVacuum);
 	}
 	
-	private void calcResistance(){
+	private void calcAirResistance(){
 		double t = T0 - mPosition.getY()*L;
 		double p = P0 * Math.pow(1-L*mPosition.getY()/T0, G*M/R/L);
 		double thickness = p*M/R/t;
+		//сопротивление воздуха
 		mAirResistance = new Point2D(-1*Cf*thickness*getSpeed().getX()*getSpeed().getX()/2*S,
 				-1*Cf*thickness*getSpeed().getY()*getSpeed().getY()/2*S);
+		//сопротивление ветра по формуле F=0.5*r*V^2*S r-плотность воздуха, V-скорость ветра
+		Point2D temp = new Point2D(0.5*thickness*mWindResistance.getX()*mWindResistance.getX()*S,
+				0.5*thickness*mWindResistance.getY()*mWindResistance.getY()*S);
+		//проверка знака у ветра
+		temp = new Point2D(mWindResistance.getX()<0 ? -temp.getX() : temp.getX(),
+				mWindResistance.getY()<0 ? -temp.getY() : temp.getY());
+		mAirResistance.add(temp);
 	}
-
 	
 	private void calcAcceleration(){
-		calcResistance();
-		mAcceleration = mGravity.add(mAirResistance.add(mWindResistance)).multiply(1.0/weight);
+		calcAirResistance();
+		mAcceleration = mGravity.add(mAirResistance).multiply(1.0/WEIGHT);
 	}
 
 	/**
