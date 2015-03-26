5dfee893dc45e8c5391fce5f6deb56da45eb085c
silentGit <silentgit@mailinator.com>
2015-03-26 20:37:23 +0500

    Формула Сиаччи

--- src/Packet53ОФ350.java
+++ src/Packet53ОФ350.java
@@ -7,7 +7,7 @@ public class Packet53ОФ350 extends Packet{
 	public final double WEIGHT = 6.2;
 
 	//характеристика среды
-	public final double Cf = 1.905; //коэффициент для вычисления сопротивления воздуха
+	public final double Cf = 0.905; //коэффициент для вычисления сопротивления воздуха
 
 	public Packet53ОФ350(Double speed) {
 		super(speed);
@@ -22,8 +22,23 @@ public class Packet53ОФ350 extends Packet{
 		//System.out.println(WEIGHT);
 	}
 
+	protected double siacci(double V){
+		double linearTerm = 0.2002 * V - 48.05;
+		double middleTerm = Math.sqrt(
+				Math.pow((0.1648*V - 47.95),2)
+						+9.6);
+		double squareTerm = 0.0442*V *(V-300) /
+				(Math.pow((V / 200), 10)
+						+ 371);
+		return linearTerm + middleTerm + squareTerm;
+	}
+
+
 	@Override
 	protected Point2D resistance(Double thickness, Point2D speed){
-		return speed.normalize().multiply(Cf*thickness*speed.magnitude()*speed.magnitude()/2*S);
+		double V = speed.magnitude();
+		double siacci = siacci(V);
+		double normal = Cf*thickness*V*V/2*S;
+		return speed.normalize().multiply(RADIUS * RADIUS * 1000 * siacci);
 	}
 }
